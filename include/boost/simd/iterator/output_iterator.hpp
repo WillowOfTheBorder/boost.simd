d0bc15c6c7d87fa9a7b494067f3cf7d494fe5369
Kenny Peou <kenny.peou@numscale.com>
2016-05-04 15:14:17 +0200

    forgot to modify header guard

--- include/boost/simd/iterator/output_iterator.hpp
+++ include/boost/simd/iterator/output_iterator.hpp
@@ -7,8 +7,8 @@
 //                 See accompanying file LICENSE.txt or copy at
 //                     http://www.boost.org/LICENSE_1_0.txt
 //==============================================================================
-#ifndef BOOST_SIMD_MEMORY_OUTPUT_ITERATOR_HPP_INCLUDED
-#define BOOST_SIMD_MEMORY_OUTPUT_ITERATOR_HPP_INCLUDED
+#ifndef BOOST_SIMD_ITERATOR_OUTPUT_ITERATOR_HPP_INCLUDED
+#define BOOST_SIMD_ITERATOR_OUTPUT_ITERATOR_HPP_INCLUDED
 
 #include <boost/simd/function/store.hpp>
 #include <boost/simd/pack.hpp>
